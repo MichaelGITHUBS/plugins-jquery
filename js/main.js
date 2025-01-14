cfa9826b69a0cc83e60a852744de896eaf498bd9
Michael <miguelmotaitz@gmail.com>
2025-01-14 08:06:56 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
+    
     $('#carousel-imagens').slick({
         autoplay: true,
         arrows: false,
         dots: true,
         infinite: true
-    });
-
-    $('.menu-hamburguer').click(function () {
-        $('nav').slideToggle();
     });
 
     $('#telefone').mask('(00) 00000-0000', {
@@ -31,18 +28,23 @@ $(document).ready(function() {
             email: {
                 required: true,
                 email: true
+                
             },
             telefone: {
                 required: true
+                
             },
             cpf: {
                 required: true
+                
             },
             endereco: {
                 required: true
+               
             },
             cep: {
                 required: true
+                
             }
         },
         messages: { 
